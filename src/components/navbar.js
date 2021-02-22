48beb2c225e17ea6194e4e7d2c986f462585aadf
Evan Morris <evanmorris97@gmail.com>
2021-02-22 16:40:17 +0000

    Revert "Revert "Navbar.js unordered list for links""
    This reverts commit b906b0e24d7a623916e1df5813c067a7745599e2.

---
+++ src/components/navbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './navbar.css';
+
+const Navbar = (props) => {
+    return (
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Explore</li>
+                <li>Notifications</li>
+                <li>Messages</li>
+                <li>Bookmarks</li>
+                <li>Lists</li>
+                <li>Profile</li>
+                <li>More</li>
+            </ul>
+        </nav>
+    )
+}
+export default Navbar;
