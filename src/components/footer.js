053424880fea69a9c4c4d8b25ee3fb15e4bb68f4
Evan Morris <evanmorris97@gmail.com>
2021-02-23 17:08:31 +0000

    added target : blank to open links in new tab

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,10 +4,14 @@ const TwitterFooter = () => {
     return(
         <div className="footerDiv">
             <ul>
-                <li><a href="https://twitter.com/en/tos">Terms of Service</a></li>
-                <li><a href="https://twitter.com/en/privacy">Privacy Policy</a></li>
-                <li><a href="https://help.twitter.com/en/rules-and-policies/twitter-cookies">Cookie Policy</a></li>
-                <li><a href="https://business.twitter.com/en/help/troubleshooting/how-twitter-ads-work.html">Ads Info</a></li>
+                <li><a href="https://twitter.com/en/tos" target="_blank">
+                    Terms of Service</a></li>
+                <li><a href="https://twitter.com/en/privacy" target="_blank">
+                    Privacy Policy</a></li>
+                <li><a href="https://help.twitter.com/en/rules-and-policies/twitter-cookies" target="_blank">
+                    Cookie Policy</a></li>
+                <li><a href="https://business.twitter.com/en/help/troubleshooting/how-twitter-ads-work.html" target="_blank">
+                    Ads Info</a></li>
                 <li><p>2021 Hackathon, Twitter</p></li>
             </ul>   
         </div>
