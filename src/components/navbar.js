1d5a1bcc381456c501001f36f35f2a58c731e89a
Adam Manley <adam.p.manley@gmail.com>
2021-02-23 11:59:18 +0000

    Added Home Logo
    added home logo and twitter logo

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,12 +6,14 @@ import Bookmarks from './bookmark-regular.svg'
 import Lists from './list-solid.svg'
 import Profile from './user-solid.svg'
 import More from './comment-dots-regular.svg'
-
+import Home from './home-solid.svg'
+import Bird from './bird.svg';
 const Navbar = () => {
     return (
         <nav>
+            <img src={Bird} alt="twitlogo" />
             <ul className="sidebar">
-                <li>Home</li>
+                <li><img src={Home} alt="Home" /></li>
                 <li><img src={Explore} alt="Explore" /></li>
                 <li><img src={Notification} alt="Notification" /></li>
                 <li><img src={Messages} alt="Messages" /></li>
