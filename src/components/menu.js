0e4ea293a5f1b7b05fabdd8e6c1a4347ca46a075
Adam Manley <adam.p.manley@gmail.com>
2021-02-25 11:13:02 +0000

    Images added to more menu

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,5 +1,14 @@
 import React, { Component } from 'react';
 import './menu.css'
+import Topics from './images/topics.svg'
+import Moments from './images/moments.svg'
+import Newsletter from './images/newsletters.svg'
+import TwitAds from './images/twitterAds.svg'
+import Analytics from './images/analytics.svg'
+import Settings from './images/privacySettings.svg'
+import HelpCenter from'./images/helpCenter.svg'
+import Display from './images/displayBrush.svg'
+import KeyboardShorts from './images/keyboardShortcuts.svg'
 
 class More extends Component {
     constructor() {
@@ -19,15 +28,15 @@ class More extends Component {
                 {this.state.showMenu
                     ? (
                         <div className='menuTop'>
-                            <button id="topics">Topics</button>
-                            <button id='moments'>Moments</button>
-                            <button>Newsletters</button>
-                            <button>Teitter Adds</button>
-                            <button>analytics</button>
-                            <button>settings and privacy</button>
-                            <button>Help Center</button>
-                            <button>Display</button>
-                            <button>Keyboard Shortcuts</button>
+                            <button id="topics"><img src={Topics}></img>Topics</button>
+                            <button id='moments'><img src={Moments}></img>Moments</button>
+                            <button id='newsletters'><img src={Newsletter}></img>Newsletters</button>
+                            <button id='twitAdds'><img src={TwitAds}></img>Twitter Ads</button>
+                            <button id='analytics'><img src={Analytics}></img>analytics</button>
+                            <button id='settingsAndPriv'><img src={Settings}></img>Settings and Privacy</button>
+                            <button id='helpCenter'><img src={HelpCenter}></img>Help Center</button>
+                            <button id='display'><img src={Display}></img>Display</button>
+                            <button id='keyboardShortcuts'><img src={KeyboardShorts}></img>Keyboard Shortcuts</button>
                         </div>
                     )
                     : (null)
