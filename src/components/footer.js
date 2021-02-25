9d4bbaf4b4925dd1cb074d67eb31472dcee71b4a
Evan Morris <evanmorris97@gmail.com>
2021-02-25 11:48:43 +0000

    footer css styling

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,17 +3,18 @@ import './footer.css';
 const TwitterFooter = () => {
     return(
         <div className="footerDiv">
-            <ul>
-                <li><a href="https://twitter.com/en/tos" target="_blank">
-                    Terms of Service</a></li>
-                <li><a href="https://twitter.com/en/privacy" target="_blank">
-                    Privacy Policy</a></li>
-                <li><a href="https://help.twitter.com/en/rules-and-policies/twitter-cookies" target="_blank">
-                    Cookie Policy</a></li>
-                <li><a href="https://business.twitter.com/en/help/troubleshooting/how-twitter-ads-work.html" target="_blank">
-                    Ads info</a></li>
-                <li><p>2021 Hackathon, Twitter</p></li>
-            </ul>   
+            <nav className="footerNavCSS">
+                <a className="termsOfService" href="https://twitter.com/en/tos" target="_blank">Terms of Service</a>
+                <a className="privacyPolicy" href="https://twitter.com/en/privacy" target="_blank">Privacy Policy</a>
+                <a className="cookiePolicy" href="https://help.twitter.com/en/rules-and-policies/twitter-cookies" target="_blank">Cookie Policy</a>
+                <a className="adsInformation" href="https://business.twitter.com/en/help/troubleshooting/how-twitter-ads-work.html" target="_blank">Ads info</a>
+                <div className="footerMoreButton">
+                    
+                </div>
+                <div className="hackathonTwitterNameFooter">
+                    <p>2021 Hackathon, Twitter</p>
+                </div>                
+            </nav>                       
         </div>
     )
 }
