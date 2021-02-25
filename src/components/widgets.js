ab2a63cc59b562aa5c5d86c2e0eda6207b188b86
Jake Dibbert <jake.dibbert@sofology.co.uk>
2021-02-25 18:24:25 +0000

    minor changes to widget css

--- src/components/widgets.js
+++ src/components/widgets.js
@@ -4,21 +4,18 @@ import WhatsHappening from './whatsHappening';
 import WhoToFollow from './whotofollow';
 import RightSectMore from './rightSectMore';
 import TwitterFooter from './footer'
-import SearchIcon from '@material-ui/icons/Search';
+
 function widgets() {
     return (
         <div className="widgets">
-          <div className="widgets_input">
-          <SearchIcon className="widgets_searchIcon" />
-          <input placeholder="Search Twitter" type="text" />
-          </div>
-        <div className ="moveRight">
+
+        <Searchbar />
+        <div className="widgetContainer">
         <WhatsHappening />
         <WhoToFollow />
         <TwitterFooter />
-        <RightSectMore /> 
-
-        </div>            
+        <RightSectMore />  
+        </div>  
         </div>
     )
 }
