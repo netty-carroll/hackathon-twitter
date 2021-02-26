0715ed978496a7324c393299649314885dceec96
Jake Dibbert <jake.dibbert@sofology.co.uk>
2021-02-26 11:38:31 +0000

    css cleanup

--- src/components/centretop.js
+++ src/components/centretop.js
@@ -7,8 +7,8 @@ const CentreTopTweets = () => {
     return(
         <div className="centreTopTweets">
             <div className="homeHeading">
-                <h2>Home</h2>
-                <img className="topTweetSearchIcon" src={TopTweetsIcon}></img>
+                <h2>Home<img className="topTweetSearchIcon" src={TopTweetsIcon}></img></h2>
+                {/* <img className="topTweetSearchIcon" src={TopTweetsIcon}></img> */}
             </div>
            <Tweetbox />
            <Post />
--- src/components/whatsHappening.js
+++ src/components/whatsHappening.js
@@ -5,8 +5,14 @@ const WhatsHappening = () => {
         <div className="whatsHapSection">
             <div className="whatsHapHeader">
                 <h2>What's happening</h2>
-                
-            </div>           
+            </div>  
+            <div className="firstHappening">
+                <h2> News - Last night </h2>
+                <p>US carries out airstrike on</p>
+                <p>Iranian-backed militia facilities in</p>
+                <p>Syria</p>
+                <img src="https://pbs.twimg.com/semantic_core_img/1365105055041392642/fbJxJRoD?format=jpg&name=240x240" />
+                </div>         
 
             <div className="whatsHapShowMore">
                 <a href="">Show more</a>
